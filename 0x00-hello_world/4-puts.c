09a286e4dc0b242471207af60232ac204ebc70d8
DanielBaquero28 <835@holbertonschool.com>
2019-06-19 21:00:17 +0000

    C program that prints a string

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * Main - It is the Principal Function
+ * main - It is the Principal Function
  *
  * Return: Always 0 (Success)
 **/
 int main(void)
 {
-puts ("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
