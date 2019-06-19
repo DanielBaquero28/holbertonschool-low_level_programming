0eef308290ac9ba395b6d4a1eebd8d76569f24cc
DanielBaquero28 <835@holbertonschool.com>
2019-06-19 20:52:47 +0000

    C program printing a string

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,10 +4,10 @@
  * Main - Función principal
  *
  * Return: Always 0 (Success)
- */
+**/
 
 int main(void)
 {
-  printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
-  return (0);
+printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
+return (0);
 }
