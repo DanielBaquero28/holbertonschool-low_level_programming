28dffc6dee9bd32207d656e6ecded507160defca
DanielBaquero28 <835@holbertonschool.com>
2019-06-19 19:24:49 +0000

    C program that prints a string

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
-/* Function main, as it is of type integer, it will return the number 0
-   Function puts prints the string as asked in the question */ 
-
+/** 
+ * Function main- As it is of type integer, it will return the number 0
+ * Return: Always 0 (Success) 
+*/ 
 int main(void)
 {
-puts ("Programming is like building a multilingual puzzle\n");
+puts ("\"Programming is like building a multilingual puzzle\n");
 return (0);
 }
