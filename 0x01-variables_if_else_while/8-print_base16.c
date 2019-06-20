e411568a459e65bb08b203a750ee70567a74f1ce
DanielBaquero28 <835@holbertonschool.com>
2019-06-20 21:03:11 +0000

    Prints all the numbers of base 16 in lowercase, followed by a new line

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+# include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: A text
+**/
+
+int main(void)
+{
+int i;
+char alphabet[16] = "0123456789abcdef";
+char letterLook;
+
+while (i < 16)
+{
+letterLook = alphabet[i];
+putchar(letterLook);
+i++;
+}
+putchar ('\n');
+
+return (0);
+}
