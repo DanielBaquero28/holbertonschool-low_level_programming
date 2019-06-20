07ad9aa22143dd6b61e620d0b9458656311284f8
DanielBaquero28 <835@holbertonschool.com>
2019-06-20 21:27:57 +0000

    Prints all possible combinations of single-digit numbers without char

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: A text
+**/
+
+int main(void)
+{
+int i;
+
+i = '0';
+
+while (i <= '9')
+{
+putchar(i);
+
+if( i != '9' )
+{
+putchar(',');
+putchar(' ');
+}
+i++;
+}
+putchar('\n');
+
+ return (0);
+} 
