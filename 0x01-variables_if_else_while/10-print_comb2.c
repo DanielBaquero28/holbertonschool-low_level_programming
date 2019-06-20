c3bd468ddb6f060fe47c30d1c50ccea05c32bcd6
DanielBaquero28 <835@holbertonschool.com>
2019-06-20 21:48:46 +0000

    Program that prints the numbers from 00 to 99

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Entrypoint 
+ * Return: 0
+**/
+
+int main(void)
+{
+int pri;
+int ult;
+pri = 48;
+ult = 48;
+while (pri < 58)
+{
+while (ult < 58)
+{
+putchar(pri);
+putchar(ult);
+if (pri != 57 ||  ult != 57)
+{
+putchar(44);
+putchar(32);
+}
+ult++;
+
+}
+ult = 48;
+pri++;
+}
+putchar('\n');
+return (0);
+}
