3d9591142c100d7746fd38a515644ce029e3b2e8
DanielBaquero28 <835@holbertonschool.com>
2019-06-24 23:16:42 +0000

    Printing the alphabet 10 times separated by a newline each time

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,14 +5,21 @@
  * main - check the code for Holberton School students.
  *
  * Return: Always 0.
- **/
-void print_alphabet_x10(void);
+**/
+void print_alphabet_x10(void)
 {
 int i;
+int l;
 
-for(i = 97; i <= 122; i++)
+l = 'a';
+for(i = 0; i < 10 ; i++)
 {
-10 * (_putchar(i));
+while( l <= 'z')
+{
+_putchar(l) ;
+l++;      
 }
 _putchar('\n');
+l = 'a';
+}
 }
