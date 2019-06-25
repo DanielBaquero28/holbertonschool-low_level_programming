07d603887e6e95dea57e930fb6f0a7591f78c4b4
DanielBaquero28 <835@holbertonschool.com>
2019-06-25 18:59:36 +0000

    Prints all natural numbers from n to 98, followed by a new line

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,13 +3,29 @@
 
 /**
  * print_to_98 - Check the code for Holberton School students.
- * @c: The character to print
+ * @n: The character to print
  *
- * Return: The last digit of a number.
+ * Return: Nothing to return
 **/
+
 void print_to_98(int n)
 {
-
-
-
+if (n < 98)
+{
+for (; n < 98; n++)
+{
+printf("%d, ", n);
+}
+}
+else if (n > 98)
+{
+for (; n > 98; n--)
+{
+printf("%d, ", n);
+}
+}
+if (n == 98)
+{
+printf("%d\n", n);
+}
 }
