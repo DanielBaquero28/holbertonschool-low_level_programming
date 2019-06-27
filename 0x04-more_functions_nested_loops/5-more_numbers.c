9440eaf354649dbb4158de2b75dcf608cbd3bef6
DanielBaquero28 <835@holbertonschool.com>
2019-06-27 16:24:50 +0000

    Function that prints 10 times the numbers, from 0 to 14, followed by a new line

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "holberton.h"
+
+/**
+ * more_numbers - Check the code for Holberton School students.
+ *
+ * Return: Nothing to return.
+**/
+void more_numbers(void)
+{
+int i;
+for (i = 0; i <= 14; i++)
+{
+_putchar(i + 48);
+}
+_putchar(10);
+}
