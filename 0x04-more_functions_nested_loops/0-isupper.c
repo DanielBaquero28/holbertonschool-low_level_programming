73363e4f52121d48da83ae9000d4b4037b57bbbf
DanielBaquero28 <835@holbertonschool.com>
2019-06-27 14:53:33 +0000

    Function that checks for uppercase character

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "holberton.h"
+
+/**
+ * _isalpha - Check the code for Holberton School students.
+ * @c: The character to print
+ *
+ * Return: Always 0.
+**/
+
+ int _isupper(int c)
+{
+for (c = 65; c <= 90; c++)
+{
+return (1);
+}
+return (0);
+}
