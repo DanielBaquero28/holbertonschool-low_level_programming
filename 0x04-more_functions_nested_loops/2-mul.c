2942fea22fe09d31ba654f76b8294177e86c329a
DanielBaquero28 <835@holbertonschool.com>
2019-06-27 15:20:22 +0000

    Functionthat multiplies 2 number

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "holberton.h"
+
+/**
+ * _isalpha - Check the code for Holberton School students.
+ * @a: The character to print
+ * @b: The character
+ *
+ * Return: Always 0.
+**/
+
+ int mul(int a, int b)
+{
+return (a * b);
+}
