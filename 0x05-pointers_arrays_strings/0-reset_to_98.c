ec9d2b47e4085f6f11660213678e2a0fd6620b75
DanielBaquero28 <835@holbertonschool.com>
2019-07-01 21:09:54 +0000

    Update the value of a pointer to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - Updates a pointer to have the value of 98
+ * @n: The character to print
+ *
+ * Return: Nothing
+**/
+
+void reset_to_98(int *n)
+{
+  *n = 98;
+}
