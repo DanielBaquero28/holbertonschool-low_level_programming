34a0b9e699551da8a976a02e43f343fd767c42ef
DanielBaquero28 <835@holbertonschool.com>
2019-07-02 18:47:18 +0000

    Function that converts a string to an integer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * print_rev - Will print a string in reverse
+ * @s: The pointer that will be printed in reverse
+ *
+ * Return: Nothing
+**/
+
+int _atoi(char *s)
+{
+
+}
