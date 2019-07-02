4bbe0e57fb542f28b08c2d948193dc44c6835a92
DanielBaquero28 <835@holbertonschool.com>
2019-07-02 02:34:43 +0000

    Function that prints a string, followed by a new line, to stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _puts - Updates a pointer to have the value of 98
+ * @a: The pointer that swaps it's value with b
+ * 
+ * Return: Nothing
+**/
+
+void _puts(char *str)
+{
+puts(str);
+}
