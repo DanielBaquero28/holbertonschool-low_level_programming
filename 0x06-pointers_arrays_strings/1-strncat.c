8cbfe2de9f680ac80da3c03a0efcb9dfc83f1444
DanielBaquero28 <835@holbertonschool.com>
2019-07-04 15:49:11 +0000

    Function thta appends two strings with not more than n bytes using pointers

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <string.h>
+
+/**
+ * _strcat - Concatenates two strings with pointers.
+ * @dest: Pointer
+ * @src: Pointer
+ * @n: Variable with number of bytes
+ * Return: Characters
+ **/
+char *_strncat(char *dest, char *src, int n)
+{
+int i, j;
+
+for (i = 0; dest[i] != 0; i++)
+{
+}
+j = 0;
+while (src[j] != '\0')
+{
+n = 0;
+if (src[j] <= n)
+dest[i] = src[j];
+j++;
+i++;
+}
+dest[i] = '\0';
+return (dest);
+}
