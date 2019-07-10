837b50e87d36f7c99d6fe165e809a3646b6d6c33
DanielBaquero28 <835@holbertonschool.com>
2019-07-10 21:17:27 +0000

    Function that checks if the input integer is a prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -3,10 +3,12 @@
 /**
  * is_prime_number - Checks if it is a prime number or not
  * @n: Pointer
+ * @i: Goes through
+ * @stops: Stops when reaches here
  * Return: If it is a prime number or not
 **/
 
-int prime_num(int n, int i, int limit);
+int prime_num(int n, int i, int stops);
 
 int is_prime_number(int n)
 {
@@ -21,12 +23,12 @@ return (prime_num(n, 2, n / 2));
  * Return: Integer
 **/
 
-int prime_num(int n, int i, int limit)
+int prime_num(int n, int i, int stops)
 {
-if ((n % i == 0 && i <= limit) || n < 0 || n == 1)
+if ((n % i == 0 && i <= stops) || n < 0 || n == 1)
 return (0);
-else if (n % i != 0 && i <= limit)
-return (prime_num(n, i + 1, limit));
+else if (n % i != 0 && i <= stops)
+return (prime_num(n, i + 1, stops));
 else
 return (1);
 }
