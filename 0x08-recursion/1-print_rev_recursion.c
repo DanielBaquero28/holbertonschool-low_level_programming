b3d6047ad44ce457a178ec6afdc93f5f85f91375
DanielBaquero28 <835@holbertonschool.com>
2019-07-10 16:51:15 +0000

    Function that prints a string in reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * _print_rev_recursion - Gets the length of the the initial substring.
+ * @s: Pointer
+ * Return: Nothing
+**/
+
+void _print_rev_recursion(char *s)
+{
+  if (*s != '\0')
+    {
+      _print_rev_recursion(s + 1);
+      _putchar(*s);
+    }
+}
