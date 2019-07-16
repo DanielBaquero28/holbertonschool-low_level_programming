2f437a295f0ee763735c22f7455ca909d10817f2
DanielBaquero28 <835@holbertonschool.com>
2019-07-16 21:59:42 +0000

    Numner 4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * free_grip - Duplicates a string to a newly allocated space in memory.
+ * @grid : Grid
+ * @height : Height
+ * Return: NULL or pointer.
+**/
+
+void free_grid(int **grid, int height)
+{
+int i;
+for (i = 0; i < height; i++)
+free(grid[i]);
+free(grid);
+}
