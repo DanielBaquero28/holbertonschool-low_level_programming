5263c85817c970a8c26b51eebbf947beacab0b8b
DanielBaquero28 <835@holbertonschool.com>
2019-07-16 18:10:43 +0000

    Fixed when cloning ptr[j]

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * _strdup - Duplicates a string to a newly allocated space in memory.
+ * @str: String duplicated
+ * Return: NULL or pointer.
+**/
+
+char *_strdup(char *str)
+{
+char *ptr;
+int i, j;
+
+if (str == NULL)
+return (NULL);
+for (i = 0; str[i] != '\0'; i++)
+{
+ptr = malloc(i * sizeof(char));
+}
+if (ptr == NULL)
+return (NULL);
+j = 0;
+while (j < i)
+{
+ptr[j] = str[j];
+j++;
+}
+return (ptr);
+free(ptr);
+}
