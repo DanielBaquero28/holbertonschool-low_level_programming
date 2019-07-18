58a99c4a50ebab68134463d2621d59fe0325ed06
DanielBaquero28 <835@holbertonschool.com>
2019-07-18 20:01:57 +0000

    Fixed [Nothing]

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -3,8 +3,25 @@
 #include "holberton.h"
 
 /**
- * malloc_checked - Allocates memory using malloc
- * @b: Positive integer
- * Return: Pointer to allocated memory.
+ * array_range - Allocates memory using malloc.
+ * @min: Minimum Value
+ * @max: Maximum Value
+ * Return: Pointer to the new allocated memory.
 **/
 
+int *array_range(int min, int max)
+{
+  int *ptr;
+  int i;
+  if (max > min)
+    return (NULL);
+  ptr = malloc(((max - min) + 1) sizeof(int));
+  if (ptr == NULL)
+    return (NULL);
+  for (i = 0; i <= (max - min); i++)
+    {
+      min++;
+      ptr[i] = min;
+    }
+  return (ptr);
+}
