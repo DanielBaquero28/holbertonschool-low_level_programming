98713c1a2d82cfdeb903e5d3ac8056521fc80825
DanielBaquero28 <835@holbertonschool.com>
2019-07-18 20:08:00 +0000

    Fixed [Betty]

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -11,17 +11,16 @@
 
 int *array_range(int min, int max)
 {
-  int *ptr;
-  int i;
-  if (max > min)
-    return (NULL);
-  ptr = malloc(((max - min) + 1) sizeof(int));
-  if (ptr == NULL)
-    return (NULL);
-  for (i = 0; i <= (max - min); i++)
-    {
-      min++;
-      ptr[i] = min;
-    }
-  return (ptr);
+int *ptr;
+int i;
+if (min > max)
+return (NULL);
+ptr = malloc(((max - min) + 1) * sizeof(int));
+if (ptr == NULL)
+return (NULL);
+for (i = 0; i <= (max - min); min++, i++)
+{
+ptr[i] = min;
+}
+return (ptr);
 }
