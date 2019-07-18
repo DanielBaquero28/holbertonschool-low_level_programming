2e065e261f4a97ba71a2ede06581db93bae60ca3
DanielBaquero28 <835@holbertonschool.com>
2019-07-18 18:42:47 +0000

    Fixed [Description function of _memset]

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,28 +10,29 @@
 **/
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-  void *memset(int *stri, char b, unsigned int size);
+void *_memset(char *stri, char b, unsigned int size);
 
-  void *ptr;
-  if (nmemb == 0 || size == 0)
-    return (NULL);
-  ptr = malloc(nmemb * size);
-  if (ptr == NULL)
-    return (NULL);
-  memset(ptr, 0, (nmemb * size));
-  return (ptr);
+void *ptr;
+if (nmemb == 0 || size == 0)
+return (NULL);
+ptr = malloc(nmemb * size);
+if (ptr == NULL)
+return (NULL);
+_memset(ptr, 0, (nmemb * size));
+return (ptr);
 }
 /**
- * memset - Counts the number of bytes.
+ * _memset - Counts the number of bytes.
  * @stri: Pointer to go through the array
  * @b: Where it it stored
  * @size: Total bytes of each word
  * Return: Bytes stored
  **/
-void *memset(int *stri, char b, unsigned int size)
+void *_memset(char *stri, char b, unsigned int size)
 {
-  unsigned int i;
-  for (i = 0; str[i] < size; i++)
-    str[i] = b;
-  return (b);
+unsigned int i;
+
+for (i = 0; i < size; i++)
+stri[i] = b;
+return (stri);
 }
