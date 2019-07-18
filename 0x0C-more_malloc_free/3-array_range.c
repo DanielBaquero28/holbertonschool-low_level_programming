0f4a36fbd59ef7c567919ef9c6efa62a12c8d697
DanielBaquero28 <835@holbertonschool.com>
2019-07-18 19:43:01 +0000

    Fixed [Nothing]

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * malloc_checked - Allocates memory using malloc
+ * @b: Positive integer
+ * Return: Pointer to allocated memory.
+**/
+
