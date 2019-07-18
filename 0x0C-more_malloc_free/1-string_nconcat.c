57ce43011c2ce3bf0b0f6f5bbe61b37fc0a2e38c
DanielBaquero28 <835@holbertonschool.com>
2019-07-18 17:44:25 +0000

    Fixed [Betty style]

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,7 +3,7 @@
 #include "holberton.h"
 
 /**
- * malloc_checked - Allocates memory using malloc
+ * string_nconcat - Allocates memory using malloc
  * @s1: String that will be concatenated to string 2.
  * @s2: String in which will be concatenated from string 1.
  * @n: Positive Integer of bytes
@@ -12,21 +12,21 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-  char *ptr;
-  unsigned int j, k;
-  if (s1 == NULL)
-    s1 = "";
-  if (s2 == NULL)
-    s2 = "";
-  for (j = 0; s1[j] != '\0'; j++)
-    ;
-  ptr = malloc(j +(n * sizeof(*s2) + 1) * sizeof(*ptr));
-if (ptr == NULL)
-  return (NULL);
+char *ptr;
+unsigned int j, k;
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
 for (j = 0; s1[j] != '\0'; j++)
-  ptr[j] = s1[j];
- for (k = 0; s2[k] != '\0' && k < n; k++, j++)
-  ptr[j] = s2[k];
- ptr[j] = '\0';
+;
+ptr = malloc(j + (n * sizeof(*s2) + 1) * sizeof(*ptr));
+if (ptr == NULL)
+return (NULL);
+for (j = 0; s1[j] != '\0'; j++)
+ptr[j] = s1[j];
+for (k = 0; s2[k] != '\0' && k < n; k++, j++)
+ptr[j] = s2[k];
+ptr[j] = '\0';
 return (ptr);
 }
