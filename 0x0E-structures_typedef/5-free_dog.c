0f97fa45c6d6c9f2c026017fa4d8a449f391c28d
DanielBaquero28 <835@holbertonschool.com>
2019-07-22 21:00:13 +0000

    Fixed [Included another free function]

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_dog - free dogs
+ * @d: Pointer
+ * Return: Nothing
+**/
+
+void free_dog(dog_t *d)
+{
+  if (d != NULL)
+    {
+      free((*d).name);
+      free((*d).age);
+      free((*d).owner);
+      free(d);
+    }
+}
