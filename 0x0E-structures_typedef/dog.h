934aaefa7c6e14a57b25bb252aaee3740df669af
DanielBaquero28 <835@holbertonschool.com>
2019-07-22 20:15:25 +0000

    Fixed [Updated header file]

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,6 +17,11 @@ char *owner;
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
+/**
+ * dog_t - New type for struct dog
+**/
+typedef struct dog dog_t;
 #endif
 
 
