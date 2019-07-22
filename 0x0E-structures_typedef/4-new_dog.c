d5a33ee14d129e675b7a563b4dd670f3dc908bc7
DanielBaquero28 <835@holbertonschool.com>
2019-07-22 20:34:57 +0000

    Fixed [Changed return value]

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,23 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_dog - Prints variable of type struct dog
+ * @name: Name dog
+ * @age: Age dog
+ * @owner: Owner dog
+ * Return: Nothing
+**/
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+  struct dog *new_dog;
+  new_dog = malloc(sizeof(struct dog));
+  if (dog == NULL)
+    return (NULL);
+  new_dog-> name = name;
+  new_dog-> age = age;
+  new_dog-> owner = owner;
+  return (new_dog); 
+}
