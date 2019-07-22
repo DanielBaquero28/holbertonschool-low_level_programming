9f87e829f392a79070461ab92ba182022b6ed359
DanielBaquero28 <835@holbertonschool.com>
2019-07-22 19:54:40 +0000

    Fixed[Removed statement if age is NULL]

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,12 +16,9 @@ if ((*d).name == NULL)
 printf("Name: (nil)\n");
 else 
 printf("Name: %s\n", (*d).name);
-if ((void *)(*d).age == NULL)
-printf("Age: (nil)\n");
-else
 printf("Age: %f\n", (*d).age);
 if ((*d).owner == NULL)
 printf("Owner: (nil)\n");
 else
-printf("Owner: %s", (*d).owner);
+printf("Owner: %s\n", (*d).owner);
 }
