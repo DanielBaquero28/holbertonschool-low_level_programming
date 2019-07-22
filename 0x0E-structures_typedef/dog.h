e2b2eb4866d09e8fe538fd839545a98050191c7f
DanielBaquero28 <835@holbertonschool.com>
2019-07-22 18:22:03 +0000

    Fixed [Created a structure]

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - Creating a structure for dog
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog's owner
+ *
+ * Description: In this structure we include the name, age, and owner of dog.
+**/
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
