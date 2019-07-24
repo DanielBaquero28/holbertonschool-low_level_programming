e69b79501f48351db1ed4610d9bd93aefeaa370c
DanielBaquero28 <835@holbertonschool.com>
2019-07-24 20:32:43 +0000

    Fixed [Not ignoring 3-main.c file]

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,40 @@
+#include "3-calc.h"
+
+/**
+ * main - Prints the calculation.
+ * @argc: Number of arguments. 
+ * @argv: Array of arguments.
+ * Return: 98, 99, 100, or correct calculation.
+ **/
+int main(int argc, char **argv)
+{
+int result;
+int arg1 = 0;
+int arg2 = 0;
+int (*ptr)(int, int);
+char operator;
+
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
+}
+arg1 = atoi(argv[1]);
+arg2 = atoi(argv[3]);
+operator = argv[2][0];
+if ((argv[2][0] == '/' || argv[2][0] == '%') && arg2 == 0)
+{
+printf("Error\n");
+exit(100);
+}
+if ((operator != '*' && operator != '+' && operator != '-'
+&& operator != '/'  && operator != '%') || argv[2][1] != '\0')
+{
+printf("Error\n");
+exit(99);
+}
+ptr = get_op_func(argv[2]);
+result = (*ptr)(a, b);
+printf("%d\n", result);
+return (0);
+}
