a3f475789585e2ecc99a6819b1bea9fd89414d9a
DanielBaquero28 <835@holbertonschool.com>
2019-07-24 18:13:35 +0000

    Fixed [Included standard library]

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -2,18 +2,18 @@
 #include <stdio.h>
 
 /**
- * print_name - Prints a name.
- * @name: What will be printed.
- * @f: Pointer that defines how the name will be printed.
+ * array_iterator - Prints a specified action of a function.
+ * @array: Array of actions
+ * @size: Size of the array.
+ * @action: Action fom function that will be executed according to the user.
  * Return: Nothing
  **/
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-  int i;
-  if (action && array)
-  for (i = 0; i < (int)size; i++)
-    {
-      action(array[i]);
-    }
+int i;
+
+if (array && action)
+for (i = 0; i < (int)size; i++)
+action(array[i]);
 }
