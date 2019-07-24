009c8ac64320fd35058357938b80e951d994fe03
DanielBaquero28 <835@holbertonschool.com>
2019-07-24 20:37:52 +0000

    Fixed [Added semicolon to the return statement]

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -5,7 +5,8 @@
 /**
  * get_op_func - Looks for the most suitable function to calculate.
  * @s: Is the operator passed as argument to the program.
- * Return: A pointer to the function that corresponds to the operator given as a parameter.
+ * Return: A pointer to the function that corresponds to the operator
+ * given as a parameter.
 **/
 
 int (*get_op_func(char *s))(int, int)
@@ -25,7 +26,7 @@ while (i <= 4)
 {
 if (*ops[i].op == *s)
 {
-return (*ops[i].f)
+return (*(ops[i]).f);
 }
 i++;
 }
