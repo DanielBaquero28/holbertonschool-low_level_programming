aaa10f31a475943f013b816938c073f0f4029bdd
DanielBaquero28 <835@holbertonschool.com>
2019-07-24 18:34:31 +0000

    Fixed [Order of the conditionals]

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * int_index - Searches for an integer.
+ * @array: Iterated array 
+ * @size: Size of the array.
+ * @cmp: Compares if it is an integer or not.
+ * Return: Nothing
+ **/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i;
+if (array && cmp)
+{
+if (size <= 0)
+return (-1);
+for (i = 0; i < (int)size; i++)
+if (cmp <= 9 && cmp >= 0)
+return (i);
+}
+return (-1);
+}
