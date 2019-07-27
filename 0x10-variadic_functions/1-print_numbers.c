ff90fff50909c7cc0132ffe49f8ace1318408fe3
DanielBaquero28 <835@holbertonschool.com>
2019-07-27 17:16:23 +0000

    Fixed [Removed the conditional if pointer is NULL]

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -15,8 +15,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 unsigned int i;
 va_list valist;
 va_start(valist, n);
-if (separator == NULL)
-return;
 for (i = 0; i < n; i++)
 {
 printf("%d", va_arg(valist, unsigned int));
