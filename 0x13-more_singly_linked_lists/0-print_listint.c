42b4065a8c1ed9361bb3cff8189f648938e7a387
DanielBaquero28 <835@holbertonschool.com>
2019-08-05 21:23:23 +0000

    Fixed [Changed current pointer to const because h was also one]

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_listint - Prints elements of a list listint_t.
+ * @h: First element of the list.
+ * Return: Number of bytes. (Count)
+**/
+
+size_t print_listint(const listint_t *h)
+{
+const listint_t *current = h;
+size_t count = 0;
+while (current != NULL)
+{
+printf("%d\n", current->n);
+current = current->next;
+count++;
+}
+return (count);
+}
