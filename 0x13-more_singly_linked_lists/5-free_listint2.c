06aaae7b1050519d2a56037983924db0cc15a39c
DanielBaquero28 <835@holbertonschool.com>
2019-08-06 21:19:58 +0000

    Fixed [Betty Style]

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Frees head and *head of the list listint_t.
+ * @head: Head of all the nodes.
+ * Return: Nothing
+**/
+
+void free_listint2(listint_t **head)
+{
+listint_t *temp, *current;
+current = *head;
+while (current != NULL && head != NULL)
+{
+temp = current;
+current = current->next;
+free(temp);
+}
+*head = NULL;
+}
