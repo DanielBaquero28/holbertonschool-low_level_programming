0257030b1a6d602ca4d0a72b6b9c5af24c65ebb5
DanielBaquero28 <835@holbertonschool.com>
2019-08-13 23:18:57 +0000

    Fixed [Write function, changed file descriptor to fd]

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,7 +4,7 @@
  * @filename: Name of the created file.
  * @text_content: Null terminated string that will be written in the new file.
  * Return: 1 if succeds else -1.
- **/
+**/
 
 int create_file(const char *filename, char *text_content)
 {
@@ -14,13 +14,12 @@ int _strlen(char *str);
 int fd, writes;
 if (filename == NULL)
 return (-1);
-fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-return (1);
+fd = open(filename, O_RDWR |O_CREAT | O_TRUNC, 0600);
 if (fd == -1)
 return (-1);
 if (text_content == NULL)
 return (1);
-writes = write(STDIN_FILENO, filename, _strlen(text_content));
+writes = write(fd, text_content, _strlen(text_content));
 if (writes == -1)
 return (-1);
 close(fd);
@@ -30,7 +29,7 @@ return (1);
  * _strlen - Finds length of a string.
  * @str: Pointer that goes through the set of characters.
  * Return: String length.
- **/
+**/
 int _strlen(char *str)
 {
 int i;
