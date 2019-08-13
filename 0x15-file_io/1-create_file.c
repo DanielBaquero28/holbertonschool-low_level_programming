f7fca94674e71d8b0195ed8b9ecffd31c31bb446
DanielBaquero28 <835@holbertonschool.com>
2019-08-13 22:41:21 +0000

    Fixed [Documentation Style]

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,20 +10,20 @@ int _strlen(char *str);
 
 int create_file(const char *filename, char *text_content)
 {
- int fd, writes;
-  if (filename == NULL)
-    return (-1);
-  fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-  return (1);
-  if (fd == -1)
-    return (-1);
-  if (text_content == NULL)
-    return (1);
-  writes = write(STDIN_FILENO, filename, _strlen(text_content));
-  if (writes == -1)
-    return (-1);
-  close(fd);
-  return (1);
+int fd, writes;
+if (filename == NULL)
+return (-1);
+fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+return (1);
+if (fd == -1)
+return (-1);
+if (text_content == NULL)
+return (1);
+writes = write(STDIN_FILENO, filename, _strlen(text_content));
+if (writes == -1)
+return (-1);
+close(fd);
+return (1);
 }
 
 /**
@@ -34,10 +34,10 @@ int create_file(const char *filename, char *text_content)
 
 int _strlen(char *str)
 {
-  int i;
-  for (i = 0; i < str[i]; i++)
-    {
+int i;
+for (i = 0; i < str[i]; i++)
+{
 
-    }
-  return (i);
+}
+return (i);
 }
