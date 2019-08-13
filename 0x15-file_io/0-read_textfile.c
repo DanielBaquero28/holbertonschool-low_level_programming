036a9bd3e7c7f3d738786c48edd78711828ef7f7
DanielBaquero28 <835@holbertonschool.com>
2019-08-13 21:53:48 +0000

    Fixed [Spaces around the asterisk]

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,30 +8,30 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-  int fd, reads, writes;
-  char *space;
-  if (filename == NULL)
-    return (0);
-  space = malloc(sizeof(char) *letters);
-  if (space == NULL)
-    return (0);
-  fd = open(filename, O_RDWR);
-  if (fd == -1)
-    {
-      free(space);
-      return (0);
-    }
-  reads = read(fd, space, letters);
-  writes = write(STDOUT_FILENO, space, reads);
-  if (writes == -1)
-    {
-      free(space);
-      return (0);
-    }
-  if (writes != reads)
-    return (0);
+int fd, reads, writes;
+char *space;
+if (filename == NULL)
+return (0);
+space = malloc(sizeof(char) * letters);
+if (space == NULL)
+return (0);
+fd = open(filename, O_RDWR);
+if (fd == -1)
+{
+free(space);
+return (0);
+}
+reads = read(fd, space, letters);
+writes = write(STDOUT_FILENO, space, reads);
+if (writes == -1)
+{
+free(space);
+return (0);
+}
+if (writes != reads)
+return (0);
 
-  free(space);
-  close(fd);
-  return (writes);
+free(space);
+close(fd);
+return (writes);
 }
