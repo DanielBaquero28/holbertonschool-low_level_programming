aa97f66c7e86618be72e66ca1fb0709dc8db60da
DanielBaquero28 <835@holbertonschool.com>
2019-08-13 23:09:17 +0000

    Fixed [Documentation Style]

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,14 +2,15 @@
 /**
  * create_file - Creates a file.
  * @filename: Name of the created file.
- * @text_context: Null terminated string that will be written in the new file.
+ * @text_content: Null terminated string that will be written in the new file.
  * Return: 1 if succeds else -1.
-**/
-
-int _strlen(char *str);
+ **/
 
 int create_file(const char *filename, char *text_content)
 {
+
+int _strlen(char *str);
+
 int fd, writes;
 if (filename == NULL)
 return (-1);
@@ -25,13 +26,11 @@ return (-1);
 close(fd);
 return (1);
 }
-
 /**
- * strlen - Finds length of a string.
+ * _strlen - Finds length of a string.
  * @str: Pointer that goes through the set of characters.
  * Return: String length.
-**/
-
+ **/
 int _strlen(char *str)
 {
 int i;
