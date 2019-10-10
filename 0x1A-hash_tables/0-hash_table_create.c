266f0405c118fd1767c5422644352f34353a2539
DanielBaquero28 <835@holbertonschool.com>
2019-10-10 21:18:26 +0000

    Fixed [As it is a double pointer I must alloc for * and **]

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,15 +8,25 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-
+	unsigned long int i;
 	hash_table_t *new_table;
 
-	new_table = malloc(sizeof(hash_table_t) * size);
+	new_table = malloc(sizeof(hash_table_t));
 	if (!new_table)
 	{
 		free(new_table);
 		return (NULL);
 	}
+
+	new_table->size = size;
+	new_table->array = malloc(sizeof(hash_table_t *) *size);
+	if (!new_table->array)
+	{
+		free(new_table->array);
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+		new_table->array[i] = 0;
 
 	return (new_table);
 }
