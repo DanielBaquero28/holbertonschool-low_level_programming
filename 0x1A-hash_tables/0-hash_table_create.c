bb84154c713361f9a99daee041bfeee3aec949b1
DanielBaquero28 <835@holbertonschool.com>
2019-10-10 19:49:25 +0000

    Fixed [Added condition if malloc failed]

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,6 +12,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *new_table;
 
 	new_table = malloc(sizeof(hash_table_t) * size);
+	if (!new_table)
+	{
+		free(new_table);
+		return (NULL);
+	}
 
 	return (new_table);
 }
