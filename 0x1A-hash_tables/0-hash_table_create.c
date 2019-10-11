ad4136e60771dcd514cac7fa093ed99bbc7b2e6f
DanielBaquero28 <835@holbertonschool.com>
2019-10-11 21:09:25 +0000

    Fixed [Code Style]

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -19,7 +19,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	}
 
 	new_table->size = size;
-	new_table->array = malloc(sizeof(hash_table_t *) *size);
+	new_table->array = malloc(sizeof(hash_table_t *) * size);
 	if (!new_table->array)
 	{
 		free(new_table->array);
