19472dd06aaed3e4e319870a07f535fda58f625d
DanielBaquero28 <835@holbertonschool.com>
2019-10-11 21:37:21 +0000

    Feature [Testing]

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -28,7 +28,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	hash_index = key_index((const unsigned char *)key, ht->size);
 	if (ht->array[hash_index] != NULL)
-		new_node->next = ht->array[hash_index];
+		new_node = ht->array[0];
 
 	ht->array[hash_index] = new_node;
 	return (1);
