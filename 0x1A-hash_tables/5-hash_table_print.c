14bc53df6c78f5f674778149b831f7aa0bcf46a9
DanielBaquero28 <835@holbertonschool.com>
2019-10-11 00:04:11 +0000

    Feature [Nested loop implementation]

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - Prints the hash table.
+ * @ht: Hash Table
+ * Return: Nothing.
+**/
+
+void hash_table_print(const hash_table_t *ht)
+{
+
+	unsigned int i, j;
+	hash_node_t *help;
+
+	if (!ht)
+		return;
+
+	printf("{");
+	j = 0;
+	for (i = 0; i < ht->size; ++i)
+	{
+		for (help = ht->array[i]; help != NULL; help = help->next)
+		{
+			if (j == 1)
+				printf(", ");
+			printf("'%s': '%s'", help->key, help->value);
+			j = 1;
+		}
+	}
+	printf("}\n");
+
+}
