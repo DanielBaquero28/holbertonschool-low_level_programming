ad09031a3ceb56e94da883576ed01f1ca9cf1770
DanielBaquero28 <835@holbertonschool.com>
2019-10-30 21:38:26 +0000

    Fixed [Re-structured the program]

--- 0x1B-sorting_algorithms/100-shell_sort.c
+++ 0x1B-sorting_algorithms/100-shell_sort.c
@@ -1,6 +1,47 @@
 #include "sort.h"
 
 /**
+ * swap - Swaps the value of two pointers.
+ * @num1: Pointer that points to number one.
+ * @num2: Pointer that points to number two.
+ * Return: Nothing
+ */
+
+void swap(int *num1, int *num2)
+{
+	int temp = 0;
+
+	temp = *num1;
+	*num1 = *num2;
+	*num2 = temp;
+}
+
+
+/**
+ * shellsort - Sorts @array with the shell algorithm.
+ * @array: Array of integers.
+ * @size: The number of integers in the array.
+ * @gap: Gap of integers.
+ * Return: Nothing
+ */
+
+void shellsort(int *array, int size, int gap)
+{
+
+	int i, j;
+
+	for (i = 0; (i + gap) < size; i++)
+	{
+		for (j = i + gap; (j - gap) >= 0; j -= gap)
+		{
+			if (array[j] < array[j - gap])
+				swap(&array[j], &array[j - gap]);
+		}
+	}
+	print_array(array, size);
+}
+
+/**
  * shell_sort - Sorts @array with the shell algorithm.
  * @array: Array of integers.
  * @size: The number of integers in the array.
@@ -9,20 +50,12 @@
 
 void shell_sort(int *array, size_t size)
 {
+	size_t gap;
 
-	int i, j, gap, help;
+	gap = 1;
+	while(gap < size)
+		gap = (gap * 3) + 1;
 
-	for (gap = size / 2; gap > 0; gap /= 2)
-	{
-		for (i = gap; i < (int)size; i += 1)
-		{
-			help = array[i];
-			for (j = i; j >= gap && array[j - gap] > help; j -= gap)
-				array[j] = array[j - gap];
-
-			array[j] = help;
-		}
-	}
-	print_array(array, size);
-
+	while ((gap = (gap - 1) / 3) > 0)
+		shellsort(array, size, gap);
 }
