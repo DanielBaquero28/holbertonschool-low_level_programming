a2e9b98aca5e0a84a05e8396d2b680efaf628b32
DanielBaquero28 <835@holbertonschool.com>
2019-12-04 02:53:59 +0000

    [Feature] Version #7

---
+++ 0x1D-binary_trees/7-binary_tree_inorder.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_inorder - In-Order Travese
+ * @tree: Tree that will be traversed
+ * @func: Function Pointer
+ * Return: Nothing
+ */
+
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree || !func)
+		return;
+
+	if (tree->left != NULL)
+		binary_tree_inorder(tree->left, func);
+
+	func(tree->n);
+	if (tree->right != NULL)
+		binary_tree_inorder(tree->right, func);
+
+}
