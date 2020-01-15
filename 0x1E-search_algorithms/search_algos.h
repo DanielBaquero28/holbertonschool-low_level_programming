0ab1fb0f04b0fc14408dd9743412eea225253360
Daniel Baquero <835@holbertonschool.com>
2020-01-15 22:09:14 +0000

    [Feature]: Updated header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,5 +1,9 @@
 #ifndef SEARCH_ALGORITHMS_H
 #define SEARCH_ALOGORITHS_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
 
 /* Prototypes */
 int linear_search(int *array, size_t size, int value);
