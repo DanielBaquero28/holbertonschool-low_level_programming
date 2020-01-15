9734a7db834a0885e9c92104d0c60ee87057083f
Daniel Baquero <835@holbertonschool.com>
2020-01-15 22:05:56 +0000

    [Feature]: Added header file

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,8 @@
+#ifndef SEARCH_ALGORITHMS_H
+#define SEARCH_ALOGORITHS_H
+
+/* Prototypes */
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
