3ba2e5b7811fa7b4302a4a8e2a5c04c78ff60218
Daniel Baquero <835@holbertonschool.com>
2020-01-16 22:25:57 +0000

    [Feature] Jump Search Algorithm v0

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,48 @@
+#include "search_algos.h"
+#include <math.h>
+
+/**
+ * jump_search - Searches for a value in a sorted array of integers
+ * @array: Array of integers
+ * @size: Size of @array
+ * @value: Value that we'll look for
+ * Return: Index of @array of the @value
+ */
+
+int min(int n1, int n2);
+
+int jump_search(int *array, size_t size, int value)
+{
+	int step = sqrt(size);
+	int p = 0;
+
+	while (array[min(step, size) - 1] < value)
+	{
+		p = step;
+		step += sqrt(size);
+		if (p >= (int)size)
+			return (-1);
+	}
+	while (array[p] < value)
+	{
+		p++;
+		if (p == min(step, size))
+			return (-1);
+	}
+	if (array[p] == value)
+		return (p);
+
+	return (-1);
+}
+
+/**
+ * min - Finds the minimum out of two numbers
+ * @n1: Number 1
+ * @n2: Number 2
+ * Return: Minimum number between @n1 and @n2
+ */
+
+int min(int n1, int n2)
+{
+	return (n1 > n2 ) ? n2 : n1;
+}
